8aea91fcbb26e629e7cd4c04e284f01c1e5666de
Justin Daniel <justinddaniel@gmail.com>
2018-02-01 21:55:56 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,6 @@
-function hideWhenClicked() {
-  document.getElementByID('hide_this').style.visibility = "hidden"
+function hideWhenClicked(event) {
+  var $target = $(event.target);
+  $target.hide();
 }
+
+$('#hide_this').click(hideWhenClicked);
